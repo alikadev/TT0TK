506dd9fbf046f79c2247499f83dd42b06fd1b87d
elvin.kuci <elvin.kuci@hes-so.ch>
2025-01-20 19:30:07 +0100

    (#1) Add multiple outputs support
    Now version 0.1.1

--- README.md
+++ README.md
@@ -6,6 +6,53 @@ _TT0TK_ or _TruthTable0 ToolKit_ is a python3 program made to simplify working
 with truth tables and linear algebra. It offers a simple interface and works
 with the `tt0` format, which is a text format with some details we'll se below.
 
+## Quick start
+
+First things first, you must have _python3_ installed
+
+``` shell
+> python3 --version
+Python 3.9.6
+```
+
+After that, you'll need to install _sympy_ with (or anything else, if it works,
+it works!)
+
+``` shell
+> python3 -m pip install sympy
+```
+
+Finally, you can run `TT0TK`:
+
+``` shell
+> python3 tt0tk.py help
+TruthTable0 TookKit - v0.1.1
+A toolkit for TT0 file format
+
+Usage: tt0tk.py <command> [args]
+Commands:
+  help [format]        | Prints help (duh)
+  formula <input.tt0>  | Get raw formulas of TT
+  simplify <input.tt0> | Get simplified formulas of TT
+```
+
+You can run the example if ou want to..
+
+``` shell
+> python3 tt0tk.py formula examples/ADR3_XOR-AND-OR.tt0
+Q0 = (~a & ~b & c) | (~a & b & ~c) | (a & ~b & ~c) | (a & b & c)
+Q1 = (a & b & c)
+Q2 = (~a & ~b & c) | (~a & b & ~c) | (~a & b & c) | (a & ~b & ~c) | (a & ~b & c) | (a & b & ~c) | (a & b & c)
+> python3 tt0tk.py simplify examples/ADR3_XOR-AND-OR.tt0
+Q0 = (a & b & c) | (a & ~b & ~c) | (b & ~a & ~c) | (c & ~a & ~b)
+Q1 = a & b & c
+Q2 = a | b | c
+```
+
+As you can see, the `AND` and `OR` have been correctly simplified. The XOR is
+still pretty much the same because there are not support for `XOR` operator for
+the moment.
+
 ## TT0 format
 
 The TT0 format is made to describe a truth table by the most understandable way 
@@ -13,38 +60,34 @@ possible (at least for me).
 
 No bloat, no specific program to handle the file, only a text file with an 
 extension and bits written in it. Each line of the file is a line of the 
-truth table. A line contains an address (binary, any length, must be the
-same length) and a bit (0 or 1).
+truth table. A line contains an address and a value (binary, any length, must 
+be the same length), separated by a single space.
 
-Example: truth table of a XOR 3
+Example: truth table of a (XOR3, AND3, OR3)
 
 ``` tt0
-000 0
-001 1
-010 1 
-011 0
-100 1
-101 0
-110 0
-111 1
+000 000
+001 101
+010 101
+011 001
+100 101
+101 001
+110 001
+111 111
 ```
 
 You can change the order of the lines in the file, it doesn't matter. For
 duplicats, the formula minterms will get duplicated.
 
 ``` tt0
-101 0
-100 1
-001 1
-010 1 
-001 1
-001 1
-110 0
-111 1
-111 1
-011 0
-001 1
-000 0
+011 001
+110 001
+000 000
+010 101
+111 111
+001 101
+100 101
+101 001
 ```
 
 ## TT0 TookKit
@@ -64,8 +107,8 @@ A toolkit for TT0 file format
 Usage: tt0tk.py <command> [args]
 Commands:
   help [format]        | Prints help (duh)
-  formula <input.tt0>  | Get raw formula of TT
-  simplify <input.tt0> | Get simplified formula of TT
+  formula <input.tt0>  | Get raw formulas of TT
+  simplify <input.tt0> | Get simplified formulas of TT
 ```
 
 Notice that you can also make the program executable on Linux and MacOS, so you
--- tt0tk.py
+++ tt0tk.py
@@ -3,30 +3,41 @@
 import sys
 from sympy.logic.boolalg import to_dnf
 
-__name__ = "TruthTable0 TookKit - v0.1.0"
+__name__ = "TruthTable0 TookKit - v0.1.1"
 __desc__ = "A toolkit for TT0 file format"
 
-def get_formula(filename):
+def get_formulas(filename):
     with open(input_path, "r") as fp:
         addrlen = -1
-        formula = ""
-        seenExpr = False
+        vallen = -1
+        # Will be set with empty strings once the value length is known
+        formulas = [] 
+        seenExpr = []
 
         for line in fp:
             (addr, val) = line[:-1].split(" ")
 
+            if len(addr) == 0 or len(val) == 0:
+                sys.exit(f"Empty address or value: {line}")
             if addrlen == -1:
                 addrlen = len(addr);
+            if vallen == -1:
+                vallen = len(val);
+                formulas = [""]*len(val)
+                seenExpr = [False]*len(val)
             if len(addr) != addrlen:
                 sys.exit("the address length changed")
+            if len(val) != vallen:
+                sys.exit("the value length changed")
 
-            if val == "1":
-                if seenExpr:
-                    formula += " | " # Add a `+`
-                seenExpr = True
-                formula += decode_minterm(addr)
+            for i in range(len(val)):
+                if val[i] == "1":
+                    if seenExpr[i]:
+                        formulas[i] += " | " # Add a `+`
+                    seenExpr[i] = True
+                    formulas[i] += decode_minterm(addr)
 
-        return formula
+        return formulas
 
 def decode_minterm(addr):
     formula = "("
@@ -47,8 +58,8 @@ def do_help():
         print(f"Usage: {program} <command> [args]")
         print("Commands:")
         print("  help [format]        | Prints help (duh)")
-        print("  formula <input.tt0>  | Get raw formula of TT")
-        print("  simplify <input.tt0> | Get simplified formula of TT")
+        print("  formula <input.tt0>  | Get raw formulas of TT")
+        print("  simplify <input.tt0> | Get simplified formulas of TT")
     elif sys.argv[2] == "format":
         print("TT0 format: Truth table description")
         print("  The TT0 format is made to describe a truth table by the most")
@@ -56,13 +67,14 @@ def do_help():
         print("   program, only a text file with an extension and with and bits")
         print("   written in it")
         print("  Each line of the file is a line of the truth table.")
-        print("  A line contains an address (binary, any length, must be the")
-        print("   same length) and a bit (0 or 1).")
-        print("  Example: truth table of a XOR")
-        print("   01 1")
-        print("   10 0")
-        print("   00 0")
-        print("   11 1")
+        print("  A line contains an address and a value (binary, any length,")
+        print("   must be the length). The address anv the value are separated")
+        print("   by a single space")
+        print("  Example: truth table of a (XOR,AND,OR)")
+        print("   00 000")
+        print("   01 101")
+        print("   10 001")
+        print("   11 111")
     else:
         sys.exit(f"Unknown help page {sys.argv[2]}")
 
@@ -85,7 +97,9 @@ elif action == "formula":
     if input_path[-4:] != ".tt0":
         sys.exit(f"Bad extension: got {input_path[-4:]}, expected .tt0")
 
-    print(get_formula(input_path))
+    formulas = get_formulas(input_path)
+    for i in range(len(formulas)):
+        print(f"Q{i} = {formulas[i]}")
 elif action == "simplify":
     if len(sys.argv) != 3:
         sys.exit(f"Unexpected number of arguments... See `{program} help`")
@@ -94,6 +108,8 @@ elif action == "simplify":
     if input_path[-4:] != ".tt0":
         sys.exit(f"Bad extension: got {input_path[-4:]}, expected .tt0")
 
-    print(to_dnf(get_formula(input_path), simplify=True))
+    formulas = get_formulas(input_path)
+    for i in range(len(formulas)):
+        print(f"Q{i} = {to_dnf(formulas[i], simplify=True)}")
 else:
     sys.exit(f"Unknown action {action}, see `{program} help`")
